63838d042de5f9730dc74124429b52c53db07167
bruno-minimac <wbrunovieira77@gmail.com>
2024-05-11 20:36:31 -0300

    added skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import { json} from 'body-parser';
+import express from "express";
+import { json } from "body-parser";
 
-const app = express()
-app.use(json())
+const app = express();
+app.use(json());
 
-app.listen(3000, () => console.log('listening on port 3000'))
+app.listen(3000, () => console.log("listening on port 3000!!!"));
