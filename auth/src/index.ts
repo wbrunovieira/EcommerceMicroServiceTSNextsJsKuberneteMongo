8879484d5cb00a208538a0b56f6df319617ef40e
bruno-minimac <wbrunovieira77@gmail.com>
2024-05-12 17:18:48 -0300

    added nginx ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,4 +4,8 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hey");
+});
+
 app.listen(3000, () => console.log("listening on port 3000!!!"));
